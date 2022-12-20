c1431bf51ebf4807d5a4afd2b8ab57ce71372f8a
choiseonmun <seonmun.choi@gmail.com>
2022-12-21 01:13:10 +0900

    2. 플레이어 움직이기

--- Sokoban/Program.cs
+++ Sokoban/Program.cs
@@ -11,10 +11,46 @@ namespace Sokoban
             Console.ResetColor();           // 컬러를 초기화한다.
             Console.CursorVisible = false;  // 커서를 숨긴다.
             Console.Title = "My Sokoban";   // 타이틀을 설정한다.
-            Console.BackgroundColor = ConsoleColor.Cyan;    // 배경색을 설정한다.
+            //Console.BackgroundColor = ConsoleColor.;    // 배경색을 설정한다.
             Console.Clear();                // 콘솔 창에 출력된 내용을 모두 지운다.
 
-            Console.WriteLine("Hello, Console Game!");
+            // 플레이어 위치 좌표
+            int playerX = 0;
+            int playerY = 0;
+
+            // 게임 루프
+            while (true)
+            {
+                // Render
+                Console.Clear();    // 이전 프레임을 지운다.
+                // 플레이어를 그려준다.
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write("P");
+
+                // ProcessInput
+                ConsoleKeyInfo currentKeyInfo = Console.ReadKey();
+
+                // Update
+                if (currentKeyInfo.Key == ConsoleKey.UpArrow)   // 위로 이동 
+                {
+                    playerY = (int)Math.Max(0, playerY - 1);
+                }
+
+                if (currentKeyInfo.Key == ConsoleKey.DownArrow) // 아래로 이동 
+                {
+                    playerY = (int)Math.Min(playerY + 1, 10);
+                }
+
+                if (currentKeyInfo.Key == ConsoleKey.LeftArrow) // 왼쪽으로 이동 
+                {
+                    playerX = (int)Math.Max(0, playerX - 1);
+                }
+
+                if (currentKeyInfo.Key == ConsoleKey.RightArrow) // 오른쪽으로 이동 
+                {
+                    playerX = (int)Math.Min(playerX + 1, 15);
+                }
+            }
         }
     }
 }
